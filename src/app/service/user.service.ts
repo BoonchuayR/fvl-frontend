29752fbcdbdcc4b2045f912e6b747787e267a88b
warayut <warayutpkg@gmail.com>
2022-08-31 06:29:02 +0700

    add uaser/list user

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { addDoc, collection, collectionData, CollectionReference, deleteDoc, doc, docData, DocumentData, Firestore, updateDoc } from '@angular/fire/firestore'
+import { addDoc, collection, collectionData, CollectionReference, deleteDoc, 
+  doc, docData, DocumentData, Firestore, Timestamp, updateDoc } from '@angular/fire/firestore'
 
-interface User{
+interface Users{
+  id: string;
+  typeUser: string;
+  firstNmae: string;
+  lastName: string;
+  userEmail: string;
+  phoneNumber: string;
+  shopName: string;
+  contactNumber: string;
+  expireDate: Timestamp;
+  firstMoney: string;
 
 }
 
@@ -17,8 +28,29 @@ export class UserService {
     this.userCollection = collection(firestore, 'users');
   }
 
-  create(users: User) {
+  create(users: Users) {
     return addDoc(this.userCollection, users);
+  }
+  getAll() {
+    return collectionData(this.userCollection, {
+      idField: 'id',
+    }) as Observable<Users[]>;
+  }
+
+  get(id: string) {
+    const userDocumentReference = doc(this.firestore, `users/${id}`);
+    return docData(userDocumentReference, { idField: 'id' });
+  }
+
+  update(users: Users) {
+
+    const userDocumentReference = doc(this.firestore,`users/${users.id}`);
+    return updateDoc(userDocumentReference, { ...users});
+  }
+
+  delete(id: string) {
+    const userDocumentReference = doc(this.firestore, `users/${id}`);
+    return deleteDoc(userDocumentReference);
   }
 
 }
