e706d609f25d5dbd02da197a65f2f70bddc66237
Boonchuay Raksom <prototype.2530@gmail.com>
2022-09-05 02:18:01 +0700

    update

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,48 +1,56 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CountToModule } from 'angular-count-to';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CountToModule } from "angular-count-to";
 
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { NgbDropdownModule, NgbAccordionModule, NgbNavModule, NgbProgressbarModule, NgbTooltipModule, NgbPopoverModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { AgmCoreModule } from '@agm/core';
+import { ScrollToModule } from "@nicky-lenaers/ngx-scroll-to";
+import { NgApexchartsModule } from "ng-apexcharts";
+import {
+  NgbDropdownModule,
+  NgbAccordionModule,
+  NgbNavModule,
+  NgbProgressbarModule,
+  NgbTooltipModule,
+  NgbPopoverModule,
+  NgbCollapseModule,
+} from "@ng-bootstrap/ng-bootstrap";
+import { AgmCoreModule } from "@agm/core";
 
-import { AdminRoutingModule } from './admin-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IconsModule } from '../pages/icons/icons.module';
+import { AdminRoutingModule } from "./admin-routing.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { IconsModule } from "../pages/icons/icons.module";
 
-import { SharedModule } from '../shared/shared.module';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { UserEditComponent } from './user/user-edit/user-edit.component';
-import { UserAddComponent } from './user/user-add/user-add.component';
-import { AnnounceListComponent } from './announce/announce-list/announce-list.component';
-import { AnnounceEditComponent } from './announce/announce-edit/announce-edit.component';
-import { AnnounceAddComponent } from './announce/announce-add/announce-add.component';
-import { AnnounceViewComponent } from './announce/announce-view/announce-view.component';
-import { MeterDashboardComponent } from './meter/meter-dashboard/meter-dashboard.component';
-import { MeterListComponent } from './meter/meter-list/meter-list.component';
-import { MeterAddComponent } from './meter/meter-add/meter-add.component';
-import { MeterEditComponent } from './meter/meter-edit/meter-edit.component';
-import { MeterViewComponent } from './meter/meter-view/meter-view.component';
-import { RepairListComponent } from './repair/repair-list/repair-list.component';
-import { RepairEditComponent } from './repair/repair-edit/repair-edit.component';
-import { RepairAddComponent } from './repair/repair-add/repair-add.component';
-import { RepairViewComponent } from './repair/repair-view/repair-view.component';
-import { ShopListComponent } from './shop/shop-list/shop-list.component';
-import { ShopAddComponent } from './shop/shop-add/shop-add.component';
-import { ShopEditComponent } from './shop/shop-edit/shop-edit.component';
-import { ShopViewComponent } from './shop/shop-view/shop-view.component';
-import { TopupListComponent } from './topup/topup-list/topup-list.component';
-import { TopupAddComponent } from './topup/topup-add/topup-add.component';
-import { TopupEditComponent } from './topup/topup-edit/topup-edit.component';
-import { TopupViewComponent } from './topup/topup-view/topup-view.component';
-import { UserViewComponent } from './user/user-view/user-view.component';
-import { ProductComponent } from './product/product.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CustomerAddComponent } from './customer/customer-add/customer-add.component';
-import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
-import { CustomerListComponent } from './customer/customer-list/customer-list.component';
-import { CustomerViewComponent } from './customer/customer-view/customer-view.component';
+import { SharedModule } from "../shared/shared.module";
+import { UserListComponent } from "./user/user-list/user-list.component";
+import { UserEditComponent } from "./user/user-edit/user-edit.component";
+import { UserAddComponent } from "./user/user-add/user-add.component";
+import { AnnounceListComponent } from "./announce/announce-list/announce-list.component";
+import { AnnounceEditComponent } from "./announce/announce-edit/announce-edit.component";
+import { AnnounceAddComponent } from "./announce/announce-add/announce-add.component";
+import { AnnounceViewComponent } from "./announce/announce-view/announce-view.component";
+import { MeterDashboardComponent } from "./meter/meter-dashboard/meter-dashboard.component";
+import { MeterListComponent } from "./meter/meter-list/meter-list.component";
+import { MeterAddComponent } from "./meter/meter-add/meter-add.component";
+import { MeterEditComponent } from "./meter/meter-edit/meter-edit.component";
+import { MeterViewComponent } from "./meter/meter-view/meter-view.component";
+import { RepairListComponent } from "./repair/repair-list/repair-list.component";
+import { RepairEditComponent } from "./repair/repair-edit/repair-edit.component";
+import { RepairAddComponent } from "./repair/repair-add/repair-add.component";
+import { RepairViewComponent } from "./repair/repair-view/repair-view.component";
+import { ShopListComponent } from "./shop/shop-list/shop-list.component";
+import { ShopAddComponent } from "./shop/shop-add/shop-add.component";
+import { ShopEditComponent } from "./shop/shop-edit/shop-edit.component";
+import { ShopViewComponent } from "./shop/shop-view/shop-view.component";
+import { TopupListComponent } from "./topup/topup-list/topup-list.component";
+import { TopupAddComponent } from "./topup/topup-add/topup-add.component";
+import { TopupEditComponent } from "./topup/topup-edit/topup-edit.component";
+import { TopupViewComponent } from "./topup/topup-view/topup-view.component";
+import { UserViewComponent } from "./user/user-view/user-view.component";
+import { ProductComponent } from "./product/product.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CustomerAddComponent } from "./customer/customer-add/customer-add.component";
+import { CustomerEditComponent } from "./customer/customer-edit/customer-edit.component";
+import { CustomerListComponent } from "./customer/customer-list/customer-list.component";
+import { CustomerViewComponent } from "./customer/customer-view/customer-view.component";
 
 @NgModule({
   declarations: [
@@ -76,7 +84,7 @@ import { CustomerViewComponent } from './customer/customer-view/customer-view.co
     CustomerAddComponent,
     CustomerEditComponent,
     CustomerListComponent,
-    CustomerViewComponent
+    CustomerViewComponent,
   ],
   imports: [
     CommonModule,
@@ -96,9 +104,9 @@ import { CustomerViewComponent } from './customer/customer-view/customer-view.co
     IconsModule,
     SharedModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAbvyBxmMbFhrzP9Z8moyYr6dCr-pzjhBE'
-    })
+      apiKey: "AIzaSyAbvyBxmMbFhrzP9Z8moyYr6dCr-pzjhBE",
+    }),
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AdminModule { }
+export class AdminModule {}
