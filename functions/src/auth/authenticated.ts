bc18fcd200a49f7bdfa2f18241481f203773aee3
Boonchuay Raksom <prototype.2530@gmail.com>
2023-10-09 23:11:29 +0700

    Tum | Test LF

--- functions/src/auth/authenticated.ts
+++ functions/src/auth/authenticated.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import * as admin from "firebase-admin";
 
 /**
@@ -8,42 +8,43 @@ import * as admin from "firebase-admin";
  * @param {any} next next.
  */
 export async function isAuthenticated(req: Request, res: Response, next: any) {
-  const {authorization} = req.headers;
+  const { authorization } = req.headers;
 
   console.log("authorization >>> ", authorization);
 
   if (!authorization) {
-    return res.status(401).send({message: "Unauthorized"});
+    return res.status(401).send({ message: "Unauthorized" });
   }
 
   console.log("headers have authorization");
 
   if (!authorization.startsWith("Bearer")) {
-    return res.status(401).send({message: "Unauthorized"});
+    return res.status(401).send({ message: "Unauthorized" });
   }
 
   console.log("Start with Bearer");
 
   const split = authorization.split("Bearer ");
   if (split.length !== 2) {
-    return res.status(401).send({message: "Unauthorized"});
+    return res.status(401).send({ message: "Unauthorized" });
   }
 
   const token = split[1];
 
   try {
-    const decodedToken:
-    admin.auth.DecodedIdToken = await admin.auth().verifyIdToken(token);
+    const decodedToken: admin.auth.DecodedIdToken = await admin
+      .auth()
+      .verifyIdToken(token);
     console.log("decodedToken", JSON.stringify(decodedToken));
     res.locals = {
-      ...res.locals, uid: decodedToken.uid,
+      ...res.locals,
+      uid: decodedToken.uid,
       role: decodedToken.role,
       email: decodedToken.email,
     };
     return next();
   } catch (err: any) {
     console.error(`${err.code} -  ${err.message}`);
-    return res.status(401).send({message: "Unauthorized"});
+    return res.status(401).send({ message: "Unauthorized" });
   }
 }
-
