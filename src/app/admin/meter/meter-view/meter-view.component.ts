513719bb77d13e0bef06acb1e599211c3e822ef5
varayut29 <imgetlikeyou@gmail.com>
2023-12-27 09:54:12 +0700

    bento | push code

--- src/app/admin/meter/meter-view/meter-view.component.ts
+++ src/app/admin/meter/meter-view/meter-view.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { IotService } from 'src/app/service/iot.service';
-import { MeterService } from 'src/app/service/meter.service';
-import { ChartType } from './dashboard.model';
-import { linewithDataChart } from './data';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IotService } from "src/app/service/iot.service";
+import { MeterService } from "src/app/service/meter.service";
+import { ChartType } from "./dashboard.model";
+import { linewithDataChart } from "./data";
+import { pipe } from "rxjs";
 
 @Component({
   selector: "app-meter-view",
@@ -11,7 +12,6 @@ import { linewithDataChart } from './data';
   styleUrls: ["./meter-view.component.scss"],
 })
 export class MeterViewComponent implements OnInit {
-
   linewithDataChart!: ChartType;
   dashedLineChart!: ChartType;
   splineAreaChart!: ChartType;
@@ -37,73 +37,232 @@ export class MeterViewComponent implements OnInit {
 
   meterId: any;
   meter: any;
-  DayList:boolean=true;
-  MonthList:boolean=false;
-  YearList:boolean=false;
+  DayList: boolean = true;
+  MonthList: boolean = false;
+  YearList: boolean = false;
+  dayList: any;
+  monthList: any;
+  yearList: any;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private meterService: MeterService,
     private iotService: IotService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    
+      this.fetchDay();
     this.meterId = this.route.snapshot.params["id"];
-    this.meterService.get(this.meterId).subscribe(meter => {
+    this.meterService.get(this.meterId).subscribe((meter) => {
       this.meter = meter;
-      this.fetchData();
-    })
-    
-    this.linewithDataChart = linewithDataChart; 
+      // this.fetchData();
+    });
+
+    this.linewithDataChart = linewithDataChart;
   }
 
-  onBtnBackClicked(): void{
+  onBtnBackClicked(): void {
     this.router.navigate([`/meter-dashboard`]);
   }
 
-  graphOfDay(){
-    this.DayList=true;
-    this.MonthList=false;
-    this.YearList=false;
+  graphOfDay() {
+    this.DayList = true;
+    this.MonthList = false;
+    this.YearList = false;
+    this.fetchDay();
   }
-  graphOfMonth(){
-    this.DayList=false;
-    this.MonthList=true;
-    this.YearList=false;
+  graphOfMonth() {
+    this.DayList = false;
+    this.MonthList = true;
+    this.YearList = false;
+    this.fetchMonth();
   }
-  graphOfYear(){
-    this.DayList=false;
-    this.MonthList=false;
-    this.YearList=true;
+  graphOfYear() {
+    this.DayList = false;
+    this.MonthList = false;
+    this.YearList = true;
+    this.fetchYear();
   }
 
   private fetchData() {
-
     this.iotService.meterReport(this.meter.storeId).subscribe((res: any) => {
       const meterData = res.DATA;
       this.linewithDataChart.series = [
         {
-          name: 'Line Voltage',
-          data: meterData.map((m: any) => { return m.LINE_VOLTAGE}),
-        }
-      ]
+          name: "Line Voltage",
+          data: meterData.map((m: any) => {
+            return m.LINE_VOLTAGE;
+          }),
+        },
+      ];
 
       this.linewithDataChart.xaxis = {
-        categories: meterData.map((m: any) => { return m.TIMESTAMP }),
+        categories: meterData.map((m: any) => {
+          return m.TIMESTAMP;
+        }),
         title: {
-          text: 'Time',
+          text: "Time",
         },
-      }
+      };
     });
 
     this.linewithDataChart.yaxis = {
       title: {
-        text: 'Volt',
+        text: "Volt",
       },
       min: 190,
       max: 240,
+    };
+  }
+
+  fetchDay() {
+    this.meterService.fetchDayGraph().subscribe((res) => {
+      var data: any;
+      console.log(res);
+      data = res;
+      this.dayList = data.DATA_RESPONSE[0].HOURLY_DATA;
+      var days: any = [];
+      var cateDays:any = [];
+      for (let i = 0; i < 31; i++) {
+        if(this.dayList[i]){
+         days.push(this.dayList[i]); 
+         cateDays.push(days.length)
+        }
+      }
+      var maxRate:any=[];
+      maxRate = days.sort((a:any,b:any)=>{
+        if(+a.ACTIVE_ENERGY>+b.ACTIVE_ENERGY){
+          return -1;
+        }
+        return 1;
+        })
+      console.log(maxRate);
+      this.linewithDataChart.series = [
+        {
+          name: "Line Voltage",
+          data:
+            days.map((res:any)=>{
+              return res.ACTIVE_ENERGY;
+            })
+        },
+      ];
+      console.log(this.linewithDataChart.series);
+      this.linewithDataChart.xaxis = {
+        categories: cateDays,
+        title: {
+          text: "Time",
+        },
+      };
+      this.linewithDataChart.yaxis = {
+        title: {
+          text: "Volt",
+        },
+        min: 0,
+        max: +maxRate[0].ACTIVE_ENERGY,
+      };
+      console.log(this.linewithDataChart.yaxis)
+    });
+  }
+
+
+  fetchMonth() {
+    this.meterService.fetchMonthGraph().subscribe((resp) => {
+      var data: any;
+      console.log(resp);
+      data = resp;
+      this.monthList = data.DATA_RESPONSE[0].DATA;
+      var month: any = [];
+      var cateMonth:any = [];
+      for (let i = 0; i <= 12; i++) {
+        if(this.monthList[i]){
+          month.push(this.monthList[i]); 
+          cateMonth.push(month.length)
+        }
+      }
+      console.log(month);
+      console.log(cateMonth);
+      var maxRate:any=[];
+      maxRate = month.sort((a:any,b:any)=>{
+        if(+a.AVG_ACTIVE_ENERGY>+b.AVG_ACTIVE_ENERGY){
+          return -1;
+        }
+        return 1;
+        })
+      console.log(maxRate);
+      this.linewithDataChart.series = [
+        {
+          name: "Line Voltage",
+          data:
+          month.map((res:any)=>{
+              return res.AVG_ACTIVE_ENERGY;
+            })
+        },
+      ];
+      console.log(this.linewithDataChart.series);
+      this.linewithDataChart.xaxis = {
+        categories: cateMonth,
+        title: {
+          text: "Time",
+        },
+      };
+      console.log(this.linewithDataChart.xaxis)
+      this.linewithDataChart.yaxis = {
+        title: {
+          text: "Volt",
+        },
+        min: 0,
+        max: +maxRate[0].AVG_ACTIVE_ENERGY,
+      };
+      console.log(this.linewithDataChart.yaxis)
+    });
+  }
+  fetchYear() {
+    this.meterService.fetchYearGraph().subscribe((response) => {
+      var data: any;
+      console.log(response);
+      data = response;
+      this.yearList = data.DATA_RESPONSE[0].DATA;
+      var year: any = [];
+    var cateYear:any = [];
+    for (let i = 2000; i < 2050; i++) {
+      if(this.yearList[i]){
+        year.push(this.yearList[i]); 
+        cateYear.push(year.length)
+      }
     }
+    var maxRate:any=[];
+    maxRate = year.sort((a:any,b:any)=>{
+      if(+a.AVG_ACTIVE_ENERGY>+b.AVG_ACTIVE_ENERGY){
+        return -1;
+      }
+      return 1;
+      })
+    console.log(maxRate);
+    this.linewithDataChart.series = [
+      {
+        name: "Line Voltage",
+        data:
+        year.map((res:any)=>{
+            return res.AVG_ACTIVE_ENERGY;
+          })
+      },
+    ];
+    console.log(this.linewithDataChart.series);
+    this.linewithDataChart.xaxis = {
+      categories: cateYear,
+      title: {
+        text: "Time",
+      },
+    };
+    this.linewithDataChart.yaxis = {
+      title: {
+        text: "Volt",
+      },
+      min: 0,
+      max: +maxRate[0].AVG_ACTIVE_ENERGY,
+    };
+    console.log(this.linewithDataChart.yaxis)
+    });
   }
 }
--- src/app/service/meter.service.ts
+++ src/app/service/meter.service.ts
@@ -15,60 +15,59 @@ import {
   getFirestore,
   query,
   updateDoc,
-  where
+  where,
 } from "@angular/fire/firestore";
 import { Meter } from "../core/models/meter.model";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-const CHECK_DUP_METER_URL = "https://us-central1-fvl-app.cloudfunctions.net/api/checkDupMeter";
-
+const CHECK_DUP_METER_URL =
+  "https://us-central1-fvl-app.cloudfunctions.net/api/checkDupMeter";
+const URL_GRAPH = `https://meter.foodvillath.com/api/controller.php`;
 @Injectable({
   providedIn: "root",
 })
 export class MeterService {
-  CSVmeter = "C:\fvl\fvl-frontend\import_meters.csv";
-  subscribe(arg0: (meters: any) => void) {  
-    throw new Error('Method not implemented.');
+  CSVmeter = "C:\fvl\fvl-frontendimport_meters.csv";
+  subscribe(arg0: (meters: any) => void) {
+    throw new Error("Method not implemented.");
   }
   private meterCollection: CollectionReference<DocumentData>;
 
-  constructor(private firestore: Firestore, private http:HttpClient) {
+  constructor(private firestore: Firestore, private http: HttpClient) {
     this.meterCollection = collection(firestore, "meter");
-    
   }
-  
-  getInfo(){
-    return this.http.get(this.CSVmeter,{responseType:'text'});
+
+  getInfo() {
+    return this.http.get(this.CSVmeter, { responseType: "text" });
   }
 
   create(meter: Meter) {
-    let $metersRef=collection(this.firestore, "meter");
+    let $metersRef = collection(this.firestore, "meter");
     return addDoc(this.meterCollection, meter);
   }
 
   getAll() {
     let $metersRef = collection(this.firestore, "meter");
-    return collectionData($metersRef, {idField: "id"}) as Observable<Meter[]>;
+    return collectionData($metersRef, { idField: "id" }) as Observable<Meter[]>;
   }
-  
+
   findMeterByStatus(meterstate: string) {
-    
-    const q = query(this.meterCollection, where("meterState", "==", meterstate));
-    
+    const q = query(
+      this.meterCollection,
+      where("meterState", "==", meterstate)
+    );
+
     return collectionData(q, {
       idField: "id",
     }) as Observable<Meter[]>;
-    
   }
 
   findMeterByuid(uid: string) {
-    
     const q = query(this.meterCollection, where("uid", "==", uid));
-    
+
     return collectionData(q, {
       idField: "id",
     }) as Observable<Meter[]>;
-    
   }
 
   get(id: string) {
@@ -92,32 +91,30 @@ export class MeterService {
   }
 
   findMeterByZone(zone: string) {
-    
     const q = query(this.meterCollection, where("zone", "==", zone));
-    
+
     return collectionData(q, {
       idField: "id",
     }) as Observable<Meter[]>;
-    
   }
-  
+
   findMeterByBooothId(boothId: string) {
-    
     const q = query(this.meterCollection, where("boothId", "==", boothId));
-    
+
     return collectionData(q, {
       idField: "id",
     }) as Observable<Meter[]>;
-    
   }
 
   checkDupMeter(boothId: string) {
     return this.http.get(`${CHECK_DUP_METER_URL}?boothId=${boothId}`);
     // return fetch(`${CHECK_DUP_METER_URL}?boothId=${boothId}`);
   }
-  
+
   async getAllMeterFromAPI() {
-    const response = await fetch("https://us-central1-fvl-app.cloudfunctions.net/api/meters");
+    const response = await fetch(
+      "https://us-central1-fvl-app.cloudfunctions.net/api/meters"
+    );
     const users = await response.json();
     return users.data;
   }
@@ -127,10 +124,89 @@ export class MeterService {
     const colRef = collection(db, "meter");
     const docsSnap = await getDocs(colRef);
     const meters: any = [];
-    docsSnap.forEach(doc => {
-      meters.push(doc.data())
-    })
+    docsSnap.forEach((doc) => {
+      meters.push(doc.data());
+    });
     return meters;
   }
+  public fetchDayGraph() {
+    const options = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
+    };
+    const body = {
+      CMD_TYPE: "METER_REPORT",
+      USER_TOKEN: "a7e1b49f6dbdd1579de1929af0d7c303",
+      DEVICE_ID: ["1121 0010666"],
+      REPORT_TYPE: ["DAILY"],
+      CMD_PARAMS: [
+        "LINE_VOLTAGE",
+        "LINE_FREQUENCY",
+        "LINE_CURRENT",
+        "ACTIVE_POWER",
+        "ACTIVE_ENERGY",
+        "TIMESTAMP",
+      ],
+      REPORT_DATATIME: {
+        DATATIME_BEGIN: "2023-12-26 00:00:00",
+        DATATIME_END: "2023-12-27 00:00:00",
+      },
+    };
+    return this.http.post(URL_GRAPH, body, options);
+  }
 
+  public fetchMonthGraph() {
+    const options = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
+    };
+    const body = {
+      CMD_TYPE: "METER_REPORT",
+      USER_TOKEN: "a7e1b49f6dbdd1579de1929af0d7c303",
+      DEVICE_ID: ["1121 0010666"],
+      REPORT_TYPE: ["MONTHLY"],
+      CMD_PARAMS: [
+        "LINE_VOLTAGE",
+        "LINE_FREQUENCY",
+        "LINE_CURRENT",
+        "ACTIVE_POWER",
+        "ACTIVE_ENERGY",
+        "TIMESTAMP",
+      ],
+      REPORT_DATATIME: {
+        DATATIME_BEGIN: "2023-12-26 00:00:00",
+        DATATIME_END: "2023-12-27 00:00:00",
+      },
+    };
+    return this.http.post(URL_GRAPH, body, options);
+  }
+
+  public fetchYearGraph() {
+    const options = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
+    };
+    const body = {
+      CMD_TYPE: "METER_REPORT",
+      USER_TOKEN: "a7e1b49f6dbdd1579de1929af0d7c303",
+      DEVICE_ID: ["1121 0010666"],
+      REPORT_TYPE: ["YEARLY"],
+      CMD_PARAMS: [
+        "LINE_VOLTAGE",
+        "LINE_FREQUENCY",
+        "LINE_CURRENT",
+        "ACTIVE_POWER",
+        "ACTIVE_ENERGY",
+        "TIMESTAMP",
+      ],
+      REPORT_DATATIME: {
+        DATATIME_BEGIN: "2023-12-26 00:00:00",
+        DATATIME_END: "2023-12-27 00:00:00",
+      },
+    };
+    return this.http.post(URL_GRAPH, body, options);
+  }
 }
